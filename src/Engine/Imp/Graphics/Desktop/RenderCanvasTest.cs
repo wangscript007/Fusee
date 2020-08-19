38220bafd4fb1350f43b3c5ba233b180928dcb9c
Sarah Busert <sarah.busert@ipm.fraunhofer.de>
2020-08-19 14:04:29 +0200

    Added static class to F-E.Imp.Graphics.Desktop for testing with cpp hosting
    also downgraded OpenTk to 3.0.1 for test purposes

---
+++ src/Engine/Imp/Graphics/Desktop/RenderCanvasTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Fusee.Engine.Imp.Graphics.Desktop
+{
+    public static class RenderCanvasTest
+    {
+
+        public delegate void ExecFusAppDelegate();
+
+        public static void ExecFusApp()
+        {
+            System.Drawing.Icon appIcon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);      
+
+            Console.WriteLine("Icon created? " + appIcon != null);
+
+            var canvasImp = new RenderCanvasImp(800, 600);
+            Console.WriteLine("Canvas created? " + canvasImp != null);
+        }
+
+    }
+}
